7d952f0c3f269e8350b3350a6de385d0adaa4e80
Staslios <stanislav.dev.stk@gmail.com>
2024-06-26 18:45:05 +0200

    init

---
+++ src/components/Button.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let buttonDesc = 'A';
+    export let buttonClass = '';
+</script>
+
+<div class:buttonClass>
+    <span>{buttonDesc}</span>
+</div>
+
+<style>
+div {
+    display: inline-block;
+    height: 38px;
+    line-height: 38px;
+    font-weight: bold;
+    font-size: 22px;
+    min-width: 38px;
+    padding: 0 2px;
+    border: 1px solid gray;
+    text-align: center;
+    margin: 1px;
+    border-radius: 4px;
+    box-shadow: 1px 1px 1px gray;
+}
+</style>
---
+++ src/components/Title.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let title = 'Shortcut explorer'
+</script>
+
+<div class='title-wrapper'>
+    <h1 class="title">{title}</h1>
+</div>
+
+<style>
+    .title-wrapper {
+        padding: 14px 22px;
+        box-shadow: 1px 1px 2px var(--col-1);
+        margin-bottom: 10px;
+    }
+    .title {
+        font-size: 3.4vw;
+        font-weight: bold;
+        color: var(--col-1);
+    }
+</style>
