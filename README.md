82549c90eed5ea0762df5f0694d8468df1379b45
Staslios <stanislav.dev.stk@gmail.com>
2024-07-04 15:48:30 +0200

    added gh-pages demo & fix css url resolve when building

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # shortcut-explorer
-All keyboard shortcuts in one place
+Keyboard explorer page made with Svelte & CSS3.  
+
+### About the project
+[Live demo](https://staslios.github.io/shortcut-explorer/) made with `gh-pages`.
--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -19,7 +19,7 @@
         box-shadow: 0 1px 1px 0 var(--color-5);
         border: none;
         padding: 8px 6px 8px 40px;
-        background: url('public/search.svg') no-repeat 4%;
+        background: url('/search.svg') no-repeat 4%;
         transition: transform .15s;
     }
 
